58b5db5b653557daaf883911e6e2fc45869c3a00
tromao <tiagomromao@gmail.com>
2023-08-09 18:21:43 +0100

    Implementation of HomeScreen with tab navigation

---
+++ shared/src/commonMain/kotlin/presentation/ui/navigation/FavoritesTab.kt
@@ -0,0 +1,33 @@
+package presentation.ui.navigation
+
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabOptions
+
+object FavoritesTab : Tab {
+
+    override val options: TabOptions
+        @Composable
+        get() {
+            val icon = rememberVectorPainter(Icons.Default.Favorite)
+
+            return remember {
+                TabOptions(
+                    index = 2u,
+                    icon = icon,
+                    title = "Favorites",
+                )
+            }
+        }
+
+    @Composable
+    override fun Content() {
+        Text("Favorite Content")
+    }
+}
--- shared/src/commonMain/kotlin/presentation/ui/navigation/HomeScreen.kt
+++ shared/src/commonMain/kotlin/presentation/ui/navigation/HomeScreen.kt
@@ -2,20 +2,26 @@ package presentation.ui.navigation
 
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.BottomAppBar
+import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.annotation.ExperimentalVoyagerApi
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.screen.ScreenKey
 import cafe.adriel.voyager.core.screen.uniqueScreenKey
+import cafe.adriel.voyager.navigator.tab.CurrentTab
 import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabDisposable
 import cafe.adriel.voyager.navigator.tab.TabNavigator
+import presentation.ui.navigation.HomeTab.title
 
 
 data class HomeScreen(
@@ -25,11 +31,46 @@ data class HomeScreen(
     override val key: ScreenKey
         get() = uniqueScreenKey
 
+    @OptIn(ExperimentalVoyagerApi::class)
     @Composable
     override fun Content() {
-        Text("Teste")
+        TabNavigator(
+            HomeTab,
+            tabDisposable = {
+                TabDisposable(
+                    navigator = it,
+                    tabs = listOf(HomeTab),
+                )
+            }
+        ) { tabNavigator ->
+            Scaffold(
+                topBar = {
+                    TopAppBar(title = { Text(text = tabNavigator.current.options.title) })
+                },
+                content = {
+                    CurrentTab()
+                },
+                bottomBar = {
+                    BottomNavigation {
+                        TabNavigationItem(HomeTab)
+                        TabNavigationItem(ProfileTab)
+                        TabNavigationItem(FavoritesTab)
+                    }
+                }
+            )
+        }
     }
+}
 
+@Composable
+private fun RowScope.TabNavigationItem(tab: Tab){
+    val tabNavigator = LocalTabNavigator.current
+
+    BottomNavigationItem(
+        selected = tabNavigator.current.key == tab.key,
+        onClick = { tabNavigator.current = tab },
+        icon = { Icon(tab.options.icon!!, tab.options.title)}
+    )
 }
 
 
